84369e8eb299cf4beef11795fe69bdf4db703a0c
Nguyen Dinh Phuoc <ndphuocc3105@gmail.com>
2024-01-18 16:50:43 +0700

    update controllers

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -68,27 +68,20 @@ let getUploadFilePage = async (req, res) => {
 
 // handle form Upload-file:
 //
-let upload = multer().single("profile_pic");
 
 let handleUploadFile = async (req, res) => {
-  upload(req, res, function (err) {
-    // req.file contains information of uploaded file
-    // req.body contains information of text fields, if there were any
-    if (req.fileValidationError) {
-      return res.send(req.fileValidationError);
-    } else if (!req.file) {
-      return res.send("Please select an image to upload");
-    } else if (err instanceof multer.MulterError) {
-      return res.send(err);
-    } else if (err) {
-      return res.send(err);
-    }
+  // req.file contains information of uploaded file
+  // req.body contains information of text fields, if there were any
+  if (req.fileValidationError) {
+    return res.send(req.fileValidationError);
+  } else if (!req.file) {
+    return res.send("Please select an image to upload");
+  }
 
-    // Display uploaded image for user validation
-    res.send(
-      `You have uploaded this image: <hr/><img src="/images/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`
-    );
-  });
+  // Display uploaded image for user validation
+  res.send(
+    `You have uploaded this image: <hr/><img src="/images/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`
+  );
 };
 
 // Other Page - About
