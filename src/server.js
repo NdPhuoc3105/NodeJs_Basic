47596baa1c705474baed85bec7fa2e98e28b135b
Nguyen Dinh Phuoc <ndphuocc3105@gmail.com>
2024-01-19 00:18:12 +0700

    middleware with ExpressJs - in servers and in routes

--- src/server.js
+++ src/server.js
@@ -3,13 +3,24 @@ import configViewEngine from "./configs/viewEngine";
 require("dotenv").config();
 import initWebRoute from "./routes/web";
 import initApiRoute from "./routes/api";
-
-// connect DB
-// import pool from "./configs/connectDB";
+var morgan = require("morgan");
 
 const app = express();
 const port = process.env.PORT || 8080;
 console.log(">>> check port:", port);
+
+//
+// myself MiddleWare
+app.use((req, res, next) => {
+  // check !valid ---> res.send()
+  console.log(">> run into my middleware");
+  console.log(req.method);
+  // else -----> next()
+  next();
+});
+
+// morgan logging
+app.use(morgan("combined"));
 
 // express config to POST method
 app.use(express.urlencoded({ extended: true }));
@@ -23,6 +34,12 @@ initWebRoute(app);
 
 // init API Route
 initApiRoute(app);
+
+//
+// 404 not found - MiddleWare
+app.use((req, res) => {
+  return res.render("404.ejs");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
